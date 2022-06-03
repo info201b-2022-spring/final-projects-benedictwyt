d35b1bc5a24aeffb41697b0bfdbc9128dce02f19
benedictwyt <ywang233@uw.edu>
2022-06-02 21:43:36 -0700

    RC

--- app.R
+++ app.R
@@ -5,27 +5,26 @@ library(fmsb)
 library(shinyWidgets)
 
 #data-----------------------------------------------------------------------------------------
-bank_VTBR_df <- read.csv("Radar Chart data/VTBR Historical Data.csv")
-  bank_VTBR_df <- select(bank_VTBR_df, Ã¯..Date, Price)
+bank_VTBR_df <- read.csv("VTBR Historical Data.csv")
+  bank_VTBR_df <- select(bank_VTBR_df, ï..Date, Price)
 
-ebank_QIWI_df <- read.csv("Radar Chart data/QIWIDR Historical Data.csv")
-  ebank_QIWI_df <- select(ebank_QIWI_df, Ã¯..Date, Price)
+ebank_QIWI_df <- read.csv("QIWIDR Historical Data.csv")
+  ebank_QIWI_df <- select(ebank_QIWI_df, ï..Date, Price)
   
-telecom_MTSS_df <- read.csv("Radar Chart data/MTSS Historical Data.csv")
-  telecom_MTSS_df <- select(telecom_MTSS_df, Ã¯..Date, Price)
+telecom_MTSS_df <- read.csv("MTSS Historical Data.csv")
+  telecom_MTSS_df <- select(telecom_MTSS_df, ï..Date, Price)
 
-oil_LKOH_df <- read.csv("Radar Chart data/LKOH Historical Data.csv")
-  oil_LKOH_df <- select(oil_LKOH_df, Ã¯..Date, Price)
+oil_LKOH_df <- read.csv("LKOH Historical Data.csv")
+  oil_LKOH_df <- select(oil_LKOH_df, ï..Date, Price)
 
-    
-airline_AFLT_df <- read.csv("Radar Chart data/AFLT Historical Data.csv")
-  airline_AFLT_df <- select(airline_AFLT_df, Ã¯..Date, Price)
+airline_AFLT_df <- read.csv("AFLT Historical Data.csv")
+  airline_AFLT_df <- select(airline_AFLT_df, ï..Date, Price)
 
 
-char_df <- merge(bank_VTBR_df, ebank_QIWI_df, by = "Ã¯..Date")
-char_df <- merge(char_df, telecom_MTSS_df, by = "Ã¯..Date")
-char_df <- merge(char_df, oil_LKOH_df, by = "Ã¯..Date")
-char_df <- merge(char_df, airline_AFLT_df, by = "Ã¯..Date")
+char_df <- merge(bank_VTBR_df, ebank_QIWI_df, by = "ï..Date")
+char_df <- merge(char_df, telecom_MTSS_df, by = "ï..Date")
+char_df <- merge(char_df, oil_LKOH_df, by = "ï..Date")
+char_df <- merge(char_df, airline_AFLT_df, by = "ï..Date")
 
 char_df <- setNames(char_df, c("Date", "Bank","Ebank","Telecom", "Oil", "Airline"))
 
